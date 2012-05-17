27e2d044f2de5a7550a9abb4e8e020cd6b7c606c
Ingmar van Dijk <ingmar.v.dijk@gmail.com>
2012-05-17 20:25:51 +0200

    Script to print all cached users in a folder

--- printAllUsers.py
+++ printAllUsers.py
@@ -1,67 +1,27 @@
 import os
-import urllib
-import urllib2
+import glob
 import json
 import sys
 
-user_url = 'http://api.twitter.com/1/users/lookup.json'
-
-users_folder = 'users/'
-
 def LoadUser(user_filename):
 	with open(user_filename) as f:
-		
+		user = json.loads(f.read())
+		return user;
 
-def GetUserJSonByName(user_names):
-	names = user_names.split(',')
-        uncached_names = []
+	raise Exception('User not found');
 
-	for name in names:
-		filename = users_folder + name + '.json'
-	
-		if os.path.exists(filename):
-			print u'[{username}] in cache'.format(username=name)
+def PrintUserJson(user):
+	print u'================================'
+	print u'{username} ({followers})'.format(username=user['screen_name'], followers=user['followers_count'])
+	print u'{description}'.format(description=user['description'])
+	print
 
-		else:
-			uncached_names.append(name)
+def PrintUsers(folder):
+	all_files = glob.glob(folder + '*.json')
+	real_files = [filename for filename in all_files if os.path.islink(filename)]
+	for filename in real_files:
+		PrintUserJson(LoadUser(filename))
 
-	if not uncached_names:
-		print 'Nothing to do...'
-		quit() 
+folder = sys.argv[1]
 
-	names_to_lookup = ','.join(['%s' % uncached_name for uncached_name in uncached_names])
-
-	users_form_fields = {
-		'screen_name' : names_to_lookup,
-		'include_entities' : 0
-	}
-
-	users_form_data = urllib.urlencode(users_form_fields)
-
-	users_request = urllib2.Request(user_url, data=users_form_data)
-
-	try:
-		users_result = urllib2.urlopen(users_request)
-
-		users_json = users_result.read()
-
-		users = json.loads(users_json)
-
-		for user in users:
-			id_filename = str(user['id']) + '.json'
-			id_filepath = users_folder + id_filename
-			filename = users_folder + user['screen_name'] + '.json'
-
-			with open(id_filepath, 'w+') as f:
-				json.dump(user, f)
-
-			os.symlink(id_filename, filename)
-			print u'[{username}] saved in cache'.format(username=user['screen_name'])
-
-	except:
-		print 'Exception: Possibly no users found?', sys.exc_info()[0] 
-
-users = sys.argv[1]
-
-GetUserJSonByName(users)
-
+PrintUsers(folder)
